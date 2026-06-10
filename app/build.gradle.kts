8ffba21007e23bef24102bba29be613a059eceff
rhecpev <hadowo1202@gmail.com>
2026-06-10 22:43:33 +0900

    첫빌드버전으로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.example.wowagoodsproject"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "2.0"
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
