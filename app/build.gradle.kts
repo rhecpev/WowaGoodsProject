92739af3c7fbbb37e93925b4c13b20fa31fc89fa
rhecpev <hadowo1202@gmail.com>
2026-08-30 02:12:15 +0900

    1.6 기능개선

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.example.wowagoodsproject"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
